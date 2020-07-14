50ea665a54df9716b0aabd5f88cf6def4c181e77
rldail <56740747+rldail@users.noreply.github.com>
2020-07-14 11:26:50 -0500

    Grocery App Week 3

---
+++ groceries/src/pages/home/home.ts
@@ -0,0 +1,87 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  title = "Grocery";
+
+  items = [
+    {
+      name: "Milk",
+      quantity: 2    
+    },
+    {
+      name: "Bread",
+      quantity: 1    
+    },
+    {
+      name: "Banana",
+      quantity: 3    
+    },
+    {
+      name: "Sugar",
+      quantity: 1    
+    },
+  ];
+
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public alertCtrl: AlertController) {
+
+  }
+
+  removeItem(item, index) {
+    console.log("Removing Item - ", item, index);
+    const toast = this.toastCtrl.create({
+      message: 'Removing Item - ' + index + " ...",
+      duration: 3000
+    });
+    toast.present();
+
+    this.items.splice(index, 1);
+  }
+
+  addItem() {
+    console.log("Adding Item");
+    this.showAddItemPrompt();
+  }
+
+  showAddItemPrompt() {
+    const prompt = this.alertCtrl.create({
+      title: 'Add Item',
+      message: "Please enter item...",
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Name'
+        },
+        {
+          name: 'quantity',
+          placeholder: 'Quantity'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: item => {
+            console.log('Saved clicked', item);
+            this.items.push(item);
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+}
